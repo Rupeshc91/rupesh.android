013d0e2b906dcce9f8b46d05ea35ff8afabde965
Rupesh Jitaram Chaudhary <rupesh.chaudhary@robosoftin.com>
2016-02-11 11:45:19 +0530

    test branch

---
+++ CustomDrawable/app/src/main/java/com/customdrawable/CustomVIew.java
@@ -0,0 +1,18 @@
+package com.customdrawable;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by rupesh on 11/2/16.
+ */
+public class CustomVIew extends View {
+    public CustomVIew(Context context) {
+        super(context);
+    }
+
+    public CustomVIew(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
